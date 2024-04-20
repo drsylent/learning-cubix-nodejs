a3632fbd909615a0f7f9f5ecf18f5702bd2f3363
Dávid Csendes <csendes.david@alerant.hu>
2024-04-20 15:48:50 +0200

    Register page

--- exam/index.js
+++ exam/index.js
@@ -4,6 +4,7 @@ const app = express();
 
 app.get('/', (req, res, next) => res.render('index.ejs', res.locals));
 app.get('/login', (req, res, next) => res.render('login.ejs', res.locals));
+app.get('/register', (req, res, next) => res.render('register.ejs', res.locals));
 app.get('/password/forgot', (req, res, next) => res.render('forgotten-password.ejs', res.locals));
 app.get('/account/email/modify', (req, res, next) => res.render('modify-email.ejs', res.locals));
 app.get('/account/password/modify', (req, res, next) => res.render('modify-password.ejs', res.locals));
