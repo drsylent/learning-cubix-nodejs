1c94c763f1cc0a4d7f5994cd5d0c945e45a8e272
Dávid Csendes <csendes.david@alerant.hu>
2024-04-20 15:40:01 +0200

    Modify email page

--- exam/index.js
+++ exam/index.js
@@ -5,6 +5,7 @@ const app = express();
 app.get('/', (req, res, next) => res.render('index.ejs', res.locals));
 app.get('/login', (req, res, next) => res.render('login.ejs', res.locals));
 app.get('/password/forgot', (req, res, next) => res.render('forgotten-password.ejs', res.locals));
+app.get('/account/email/modify', (req, res, next) => res.render('modify-email.ejs', res.locals));
 
 app.listen(8080, function () {
     console.log('Running on :8080');
