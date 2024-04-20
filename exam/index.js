da85a74dc19d9bfa5162b0ab6d4ab77cad631461
Dávid Csendes <csendes.david@alerant.hu>
2024-04-20 15:28:31 +0200

    Forgotten password page

--- exam/index.js
+++ exam/index.js
@@ -4,6 +4,7 @@ const app = express();
 
 app.get('/', (req, res, next) => res.render('index.ejs', res.locals));
 app.get('/login', (req, res, next) => res.render('login.ejs', res.locals));
+app.get('/password/forgot', (req, res, next) => res.render('forgotten-password.ejs', res.locals));
 
 app.listen(8080, function () {
     console.log('Running on :8080');
