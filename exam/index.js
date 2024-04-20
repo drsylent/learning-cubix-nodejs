9688711f510a40a968341516072b57e5061e0a5d
Dávid Csendes <csendes.david@alerant.hu>
2024-04-20 16:18:57 +0200

    Tweet writing page

--- exam/index.js
+++ exam/index.js
@@ -10,6 +10,7 @@ app.get('/register', (req, res, next) => res.render('register.ejs', res.locals))
 app.get('/password/forgot', (req, res, next) => res.render('forgotten-password.ejs', res.locals));
 app.get('/account/email/modify', (req, res, next) => res.render('modify-email.ejs', res.locals));
 app.get('/account/password/modify', (req, res, next) => res.render('modify-password.ejs', res.locals));
+app.get('/tweet', (req, res, next) => res.render('tweet.ejs', res.locals));
 
 app.listen(8080, function () {
     console.log('Running on :8080');
