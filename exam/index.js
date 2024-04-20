6943e170cb79ca1df5e859b7929884fd28291c3b
Dávid Csendes <csendes.david@alerant.hu>
2024-04-20 15:24:33 +0200

    Login page

--- exam/index.js
+++ exam/index.js
@@ -3,6 +3,7 @@ import express from "express";
 const app = express();
 
 app.get('/', (req, res, next) => res.render('index.ejs', res.locals));
+app.get('/login', (req, res, next) => res.render('login.ejs', res.locals));
 
 app.listen(8080, function () {
     console.log('Running on :8080');
