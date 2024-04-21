35c2d5fdd7fe4c291c719a363dbb7d9dd6d5be6f
Dávid Csendes <csendes.david@alerant.hu>
2024-04-21 12:57:08 +0200

    Warning text can come from query string

--- exam/middleware/error/authorize.js
+++ exam/middleware/error/authorize.js
@@ -1,8 +1,10 @@
+import querystring from 'querystring';
+
 const errorMessage = 'authorize';
 
 const authorize = (err, req, res, next) => {
     if (err.message === errorMessage) {
-        return res.redirect('/login'); 
+        return res.redirect('/login?' + querystring.encode({ warning: 'Ehhez az oldalhoz előbb be kell jelentkezz' })); 
     }
     return next(err);
 }
--- exam/middleware/view/render.js
+++ exam/middleware/view/render.js
@@ -1,5 +1,10 @@
 function render(page) {
-    return (req, res, next) => res.render(page, res.locals);
+    return (req, res, next) => {
+        if (!res.locals.warning) {
+            res.locals.warning = req.query.warning;
+        }
+        return res.render(page, res.locals);
+    };
 }
 
 export { render };
