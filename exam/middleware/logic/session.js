288d845ccc0e8856a632e804b85b5339df887d3b
Dávid Csendes <csendes.david@alerant.hu>
2024-04-21 18:10:18 +0200

    Session handling

--- exam/middleware/logic/session.js
+++ exam/middleware/logic/session.js
@@ -1,3 +1,5 @@
+import session from "express-session";
+
 const mockedUserNameSetSession = (req, res, next) => {
     req.session = { userName: "test" };
     console.log('Session username set as "test" for mocking purposes');
@@ -10,5 +12,12 @@ const mockedEmptySession = (req, res, next) => {
     return next();
 };
 
+const trueSession = session({
+    secret: 'somesecret',
+    resave: false,
+    saveUninitialized: true,
+});
+
 // export { mockedUserNameSetSession as session };
-export { mockedEmptySession as session };
+// export { mockedEmptySession as session };
+export { trueSession as session };
