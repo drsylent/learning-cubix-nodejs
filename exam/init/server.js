90b035ef2db503b467c0dcb519e0b1bbbf335d16
Dávid Csendes <csendes.david@alerant.hu>
2024-04-21 11:48:47 +0200

    Added all endpoints (at least as comment)

--- exam/init/server.js
+++ exam/init/server.js
@@ -3,14 +3,29 @@ import express from "express";
 function initRoutes(app, middlewares) {
     app.get('/', middlewares.render.main);
     app.get('/login', middlewares.render.login);
-    app.get('/register',  middlewares.render.register);
-    app.get('/account/followed/tweets',  middlewares.render.listTweets);
-    app.get('/account/followed/users',  middlewares.render.listUsers);
-    app.get('/password/forgot',  middlewares.render.forgottenPassword);
-    app.get('/account/email/modify',  middlewares.render.modifyEmail);
-    app.get('/account/password/modify',  middlewares.render.modifyPassword);
-    app.get('/tweet',  middlewares.render.tweet);
-    app.get('/error',  middlewares.render.error);
+    // app.post('/login');
+    // app.post('/logout');
+    app.get('/register', middlewares.render.register);
+    // app.post('/register');
+    app.get('/password/forgot', middlewares.render.forgottenPassword);
+    // app.post('/password/forgot');
+    app.get('/password/modify/:secret', middlewares.render.forgottenPassword);
+    // app.post('/password/modify/:secret');
+    app.get('/users', middlewares.render.listUsers);
+    app.get('/:userName/tweets', middlewares.render.listTweets);
+    app.get('/account/followed/tweets', middlewares.render.listTweets);
+    app.get('/account/followed/users', middlewares.render.listUsers);
+    // app.post('/account/follow/:userName');
+    // app.post('/account/unfollow/:userName');
+    app.get('/tweet', middlewares.render.tweet);
+    // app.post('/tweet');
+    // app.post('/tweet/:tweetId/delete');
+    app.get('/account/email/modify', middlewares.render.modifyEmail);
+    // app.post('/account/email/modify');
+    // app.get('/email/modify/:secret');
+    app.get('/account/password/modify', middlewares.render.modifyPassword);
+    // app.post('/account/password/modify');
+    app.get('/error', middlewares.render.error);
 }
 
 function initServer(middlewares) {
