8877b5b0d26e9ae719d94d39bc09222e63c580c7
Dávid Csendes <csendes.david@alerant.hu>
2024-04-27 20:05:19 +0200

    Fix incorrect redirection per userName

--- exam/middleware/view/redirect.js
+++ exam/middleware/view/redirect.js
@@ -1,7 +1,7 @@
 function redirect(path) {
     return (req, res, next) => {
         if (path.includes(':userName')) {
-            path.replace(':userName', req.session.userName);
+            path = path.replace(':userName', req.session.userName);
         }
         return res.redirect(path); 
     };
