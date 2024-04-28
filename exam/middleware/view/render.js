abb8b327062bb30fd6b2810ad27cf41382fb6884
Dávid Csendes <csendes.david@alerant.hu>
2024-04-28 17:26:52 +0200

    Refactored header disabled based on signed in status

--- exam/middleware/view/render.js
+++ exam/middleware/view/render.js
@@ -4,12 +4,6 @@ function render(page) {
     return (req, res, next) => {
         res.locals.warning = getWarning(req.session);
         clearWarning(req.session);
-        if (req.session.userName) {
-            res.locals.disabled = "";
-        }
-        else {
-            res.locals.disabled = "disabled";
-        }
         res.locals.secret = req.params.secret;
         res.locals.signedInUserName = req.session.userName;
         return res.render(page, res.locals);
