8bf6a8b39cd6c53504aa679ed12e6eb3f0cfbf02
Dávid Csendes <csendes.david@alerant.hu>
2024-05-04 17:25:20 +0200

    Use promisify everywhere

--- exam/src/db/lokijs.js
+++ exam/src/db/lokijs.js
@@ -1,36 +1,20 @@
+import { promisify } from "node:util";
 import loki from "lokijs";
 import { configValue } from "../utility/config.js";
 import { logging } from "../utility/logging.js";
 
 const logger = logging("db/lokijs");
 
-function loadDatabase() {
-    return new Promise((resolve, reject) => {
-        const databaseLocation = configValue("DATABASE_LOCATION");
-        logger.info("Database location: " + databaseLocation);
-        const db = new loki(databaseLocation);
-        db.loadDatabase({}, (err) => {
-            if (err) {
-                reject(err);
-            }
-            else {
-                resolve(db);
-            }
-        });
-    });
+async function loadDatabase() {
+    const databaseLocation = configValue("DATABASE_LOCATION");
+    logger.info("Database location: " + databaseLocation);
+    const db = new loki(databaseLocation);
+    await promisify(db.loadDatabase).bind(db)({});
+    return db;
 }
 
-function saveDatabase(db) {
-    return new Promise((resolve, reject) => {
-        db.saveDatabase((err) => {
-            if (err) {
-                reject(err);
-            }
-            else {
-                resolve();
-            }
-        });
-    });
+async function saveDatabase(db) {
+    await promisify(db.saveDatabase).bind(db)();
 }
 
 export { loadDatabase, saveDatabase };
--- exam/src/middleware/logic/logout.js
+++ exam/src/middleware/logic/logout.js
@@ -6,9 +6,8 @@ const logger = logging('middleware/logic/logout');
 const logout = async (req, res, next) => {
     logger.traceWithParameters('MW called', req, res);
     const userName = req.session.userName;
-    const destroy = promisify(req.session.destroy).bind(req.session);
     try {
-        await destroy();
+        await promisify(req.session.destroy).bind(req.session)();
         logger.info("Logged out user: " + userName);
     } catch (err) {
         logger.error("Error happened during session destroying", err);
