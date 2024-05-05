8b7a888b7053066c584c39c318452740c045a97f
Dávid Csendes <csendes.david@alerant.hu>
2024-05-05 12:06:02 +0200

    Fix incorrect middleware initialization for listFollows

--- exam/src/middleware/logic/listFollows.js
+++ exam/src/middleware/logic/listFollows.js
@@ -2,15 +2,13 @@ import { logging } from "../../utility/logging.js";
 
 const logger = logging('middleware/logic/listFollows');
 
-function listFollows() {
-    return (req, res, next) => {
-        logger.traceWithParameters('MW called', req, res);
-        res.locals.users = res.locals.user.follows.map(userName => ({
-            userName,
-            currentlyFollows: true
-        }));
-        return next();
-    };
-}
+const listFollows = (req, res, next) => {
+    logger.traceWithParameters('MW called', req, res);
+    res.locals.users = res.locals.user.follows.map(userName => ({
+        userName,
+        currentlyFollows: true
+    }));
+    return next();
+};
 
 export { listFollows };
